2af6850c4d9956008e4761cfc3a5d6eaf88e2cd9
Daniel Djinishian de Briquez <dbriquez.devmaua@gmail.com>
2024-09-09 22:55:51 -0300

    semantic routing

--- chatbot_metro/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-#path authentication/url
-from rest_framework.authtoken import views
-from django.urls import path
-from .views import auth
-urlpatterns = [
-    path('get-auth/', views.obtain_auth_token),
-    path('register/', auth )
-]
--- chatbot_metro/chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path('chat/', views.chat ),
-    path('chat/delete/', views.delete_conversation),
-    path('chat/get-titles', views.get_title),
-    path('chat/get-data/', views.get_data)
-]
