cadb0ef7c10d8f5de6aa1e9cfbdedd2faf1d58c4
Carlo <carloggiacometti77@gmail.com>
2024-10-06 22:15:21 -0300

    Lógica para receber e processar mensagens de IA

--- chat_channels/MetroChatApp/consumers.py
+++ chat_channels/MetroChatApp/consumers.py
@@ -1,4 +1,5 @@
 import json
+from MetroChatApp.ia_scripts.llm import simula_resposta_ia
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from MetroChatApp.models import *
@@ -10,36 +11,34 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = f"room_{self.scope['url_route']['kwargs']['room_name']}"
         await self.accept()
+        
     # Desconecta "cliente"
     async def disconnect(self, close_code):
+        # Lógica por tempo (2 horas) inserir aqui possivelmente
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
     # Método receive (GET do javascript Form botão 'submit')
+    # Processa a 'message' e ativa resposta da IA
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json
+        
+        # Simulador de resposta de IA
+        resposta_ia = simula_resposta_ia(message)
 
-        event = {
-            'type': 'send_message', #Nome do método chamado
+        # Enviar mensagem do usuário no front
+        await self.send(text_data=json.dumps({
             'message': message,
-        }
+            'sender': 'user'
+        }))
 
-        # Para cada usuário existente na room mandamos a função de enviar msg
-        # OBS: no futuro provavelmente n precisaremos disso (chat de user e IA, somente)
-        await self.channel_layer.group_send(self.room_name, event)
-
-    async def send_message(self, event):
-        # Recebe a msg e data_json
-        data = event['message']
-        # Cria a mensagem baseada nos dados recebidos do front(js)
-        await self.create_message(data=data)
-        # Cria dic e manda para todos os usuário conectados na room
-        # OBS: no futuro provavelmente n precisaremos disso (chat de user e IA, somente)
-        response_data = {
-            'sender': data['sender'],
-            'message': data['message']
-        }
-        await self.send(text_data=json.dumps({'message': response_data}))
+        # Enviar resposta da IA
+        await self.send(text_data=json.dumps({
+            'message': {
+                'message': resposta_ia,
+                'sender': 'IA'
+            }
+        }))
     
     # "Falando" para o django que estamos praticando manipulação de dados 
     @database_sync_to_async
