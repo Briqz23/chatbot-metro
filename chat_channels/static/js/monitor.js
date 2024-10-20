39eb2a6572384b6cb7e87bc657750bbd1e25c980
Carlo <carloggiacometti77@gmail.com>
2024-10-19 21:18:16 -0300

    Adição da função de inatividade para o web app

---
+++ chat_channels/static/js/monitor.js
@@ -0,0 +1,29 @@
+let timer 
+let contagem 
+
+function resetTimer() {
+    clearTimeout(timer) // Limpa contagens anteriores
+    clearInterval(contagem)
+
+    let timeLeft = 60 // segundos
+
+    // Contador DEBUG
+    contagem = setInterval(() => {
+        console.log(`${timeLeft}`)
+        timeLeft--
+    }, 1000)
+
+    // Timer 1 minuto
+    timer = setTimeout(() => {
+        clearInterval(contagem)
+        socket.send(JSON.stringify({ 'action': 'close' }))
+        socket.close() 
+        window.location.href = '/'
+        console.log("Usuário desconectado por inatividade!")
+    }, 20000)
+}
+
+window.onload = resetTimer
+document.onmousemove = resetTimer
+document.onkeypress = resetTimer
+document.onclick = resetTimer
