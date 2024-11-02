260305c03fa914ead54efd1067a9091a073b8927
Briqz23 <dbriquez.devmaua@gmail.com>
2024-11-02 18:00:13 -0300

    realocação de arquivos ia

--- chat_channels/MetroChatApp/ia_scripts/llm.py
+++ chat_channels/MetroChatApp/ia_scripts/llm.py
@@ -1,8 +1,32 @@
 # Script chamável
-import random
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from openai import OpenAI
 
 def simula_resposta_ia(message):
-    res = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for i in range(10))
+    load_dotenv() 
+    client = OpenAI()
+    model = ChatOpenAI(
+        model='gpt-3.5-turbo-1106',
+        temperature=0.7
+        )   
+    
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": message
+        }
+     ]  
+    )
+
+    res = (completion.choices[0].message)
+    print(str(res))
     return res
 
-# print(simula_resposta_ia('prompt'))
+simula_resposta_ia('oi')
+
+
+
