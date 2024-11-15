1fb157bdeba4c0e78b1c8a7d9f81d3e69d4361af
Briqz23 <dbriquez.devmaua@gmail.com>
2024-11-15 10:00:07 -0300

    fazendo tempo de inferência mais rápido

--- chat_channels/MetroChatApp/ia_scripts/llm.py
+++ chat_channels/MetroChatApp/ia_scripts/llm.py
@@ -15,12 +15,16 @@ from .data.templates import (
 )
 from langchain.schema import Document
 
+client = OpenAI()
+model = ChatOpenAI(model="o1-mini-2024-09-12", temperature=0.7)
+
+
 def gerando_data():
     # Diretório do arquivo atual (llm.py)
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    
+
     # Caminho para o arquivo scrapping_metro.txt
-    file_path = os.path.join(current_dir, 'data', 'scrapping_metro.txt')
+    file_path = os.path.join(current_dir, "data", "scrapping_metro.txt")
 
     splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
 
@@ -30,13 +34,12 @@ def gerando_data():
     document = Document(page_content=docs)
 
     # Agora você pode usar o splitter
-    splitted_data = splitter.split_documents([document])  # Passa uma lista de documentos
+    splitted_data = splitter.split_documents(
+        [document]
+    )  # Passa uma lista de documentos
 
     return splitted_data
 
-import numpy as np
-import faiss
-import os
 
 def gerando_embedding(splitted_data):
     embedding = OpenAIEmbeddings()
@@ -58,8 +61,14 @@ def gerando_embedding(splitted_data):
         vectorStore = FAISS.from_documents(splitted_data, embedding)
         vectorStore.save_local(faiss_index_path)
 
-    # Carregar ou gerar embeddings específicos para as perguntas
-    if all(os.path.exists(path) for path in [rotas_embeddings_path, horarios_embeddings_path, politicas_embeddings_path]):
+    if all(
+        os.path.exists(path)
+        for path in [
+            rotas_embeddings_path,
+            horarios_embeddings_path,
+            politicas_embeddings_path,
+        ]
+    ):
         rotas_perguntas_embeddings = np.load(rotas_embeddings_path)
         horarios_perguntas_embeddings = np.load(horarios_embeddings_path)
         politicas_perguntas_embeddings = np.load(politicas_embeddings_path)
@@ -67,13 +76,12 @@ def gerando_embedding(splitted_data):
         rotas_perguntas_embeddings = embedding.embed_documents(rotas_perguntas)
         horarios_perguntas_embeddings = embedding.embed_documents(horarios_perguntas)
         politicas_perguntas_embeddings = embedding.embed_documents(politicas_perguntas)
-        
-        # Salvar embeddings em arquivos
+
         np.save(rotas_embeddings_path, rotas_perguntas_embeddings)
         np.save(horarios_embeddings_path, horarios_perguntas_embeddings)
         np.save(politicas_embeddings_path, politicas_perguntas_embeddings)
 
-    retriever = vectorStore.as_retriever(search_kwargs={"k": 5})
+    retriever = vectorStore.as_retriever(search_kwargs={"k": 1})
 
     return (
         retriever,
@@ -81,6 +89,7 @@ def gerando_embedding(splitted_data):
         horarios_perguntas_embeddings,
         politicas_perguntas_embeddings,
     )
+
 
 def prompt_router(
     embeddings,
@@ -91,9 +100,15 @@ def prompt_router(
 ):
     query_embedding = embeddings.embed_query(input["query"])
 
-    rotas_similarity = cosine_similarity([query_embedding], rotas_perguntas_embeddings)[0]
-    horarios_similarity = cosine_similarity([query_embedding], horarios_perguntas_embeddings)[0]
-    politicas_similarity = cosine_similarity([query_embedding], politicas_perguntas_embeddings)[0]
+    rotas_similarity = cosine_similarity([query_embedding], rotas_perguntas_embeddings)[
+        0
+    ]
+    horarios_similarity = cosine_similarity(
+        [query_embedding], horarios_perguntas_embeddings
+    )[0]
+    politicas_similarity = cosine_similarity(
+        [query_embedding], politicas_perguntas_embeddings
+    )[0]
 
     max_similarity = max(
         max(rotas_similarity), max(horarios_similarity), max(politicas_similarity)
@@ -106,57 +121,56 @@ def prompt_router(
     else:
         template = system_politicas_template
 
-    return f"{template}\n\nUsuário: {input['query']}" 
+    return f"{template}\n\nUsuário: {input['query']}"
 
 
 def simula_resposta_ia(message, chat_history):
-    client = OpenAI()
-    model = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.7)
 
-    formatted_history = [{"role": msg["role"], "content": msg["content"]} for msg in chat_history["chat_history"]]
-    
+    formatted_history = [
+        {"role": msg["role"], "content": msg["content"]}
+        for msg in chat_history["chat_history"]
+    ]
+
     completion = client.chat.completions.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "Você está falando com um assistente virtual."},
-            {"role": "user", 
-             "content": f"""
-                    CONSIDERANDO O SEGUINTE HISTÓRIO DE MENSAGENS:
+            {
+                "role": "system",
+                "content": "Você está falando com um assistente virtual.",
+            },
+            {
+                "role": "user",
+                "content": f"""
+                    --- CONSIDERANDO O SEGUINTE HISTÓRIO DE MENSAGENS:
                     {formatted_history}
-                    RESPONDA A MENSAGEM DO USUÁRIO:
+
+                    --- RESPONDA A MENSAGEM DO USUÁRIO:
                     {message}
 
-                """
-},  # Mensagem do usuário
+                """,
+            },
         ],
     )
 
     res = completion.choices[0].message.content
     return res
 
-    # Formate o histórico para o modelo da IA
-    
-    # Obtenha a resposta da IA, considerando o contexto
-    #resposta = obter_resposta_ia({"query": chat_data["chat_history"][-1]["content"], "history": formatted_history})
 
 def get_input_query(message):
     return {"query": message}
 
 
+(
+    retriever,
+    rotas_perguntas_embeddings,
+    horarios_perguntas_embeddings,
+    politicas_perguntas_embeddings,
+) = gerando_embedding(gerando_data())
+
+
 def obter_resposta_ia(message, chat_history):
-    # Gerar dados e embeddings
-    splitted_data = gerando_data()
-    (
-        retriever,
-        rotas_perguntas_embeddings,
-        horarios_perguntas_embeddings,
-        politicas_perguntas_embeddings,
-    ) = gerando_embedding(splitted_data)
 
-    # Obter a consulta de entrada
     input_query = get_input_query(message)
-
-    # Roteamento do prompt
     template = prompt_router(
         OpenAIEmbeddings(),
         input_query,
@@ -164,8 +178,5 @@ def obter_resposta_ia(message, chat_history):
         horarios_perguntas_embeddings,
         politicas_perguntas_embeddings,
     )
-
-    # Simulação de resposta da IA
     response = simula_resposta_ia(template, chat_history)
-
     return response
